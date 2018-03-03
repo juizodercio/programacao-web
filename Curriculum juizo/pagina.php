def9ec995b19ad89607f43e5defb8b104aa59575
juizodercio <31247147+juizodercio@users.noreply.github.com>
2018-03-03 06:19:30 +0200

    Add files via upload

---
+++ Curriculum juizo/pagina.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <title>Curriculum Vitae</title>
+    <link rel="stylesheet" type="text/css"href="estilo.css">
+</head>
+
+<body >
+
+
+<div>
+
+
+
+    <h1>Curriculum Vitae</h1>
+    <ul id="menu" type="circle">
+        <li><a href="#db">Dados Pessoais</a> </li><br>
+        <li><a href="#Fa">Formacao Academica</a></li><br>
+        <li><a href="#Li">Linguas Faladas</a></li><br>
+        <li><a href="#rd">Residencia</a></li><br>
+    </ul>
+    <figure class="foto">
+        <img src="imagens/juizo.png">
+
+    </figure>
+
+
+    <nav id="dadosPessoais">
+        <h2 id="db" >Dados pessoais</h2>
+        <b>Apelido</b>: juizo<br><br>
+        <b>nome</b>: Dercio jose<br><br>
+        <b>Data de nasciomento</b>: 05 de Outubro de 1997<br><br>
+        <b>Naturalidade</b>:inhamabane <br><br>
+        <b>Nacionalidade</b>: Moçambicana<br><br>
+        <b>Estado civil</b>: Solteiro<br><br>
+    </nav>
+
+    <nav id="residencia">
+        <h2 id="rd">Habilidades Profissionais</h2>
+        <b>Conhecimento em programacao java</b><br><br>
+        <b>Conhecimento em programacao web</b> <br ><br>
+
+
+    </nav>
+
+
+
+
+    <nav id="linguas">
+        <h2 id="Li">
+            Linguas Faladas
+
+        </h2>
+
+        <table border="1">
+            <tr>
+                <th>Lingua</th>
+
+                <th>Escrita</th>
+                <th>Leitura</th>
+                <th>Fala</th>
+
+
+            </tr>
+            <tr>
+                <td>Português</td>
+                <td>Excelente</td>
+                <td>Excelente</td>
+                <td> Excelente </td>
+
+
+            </tr>
+            <tr>
+                <td>Ingleês </td>
+                <td>Razoavel</td>
+                <td>Razoavel</td>
+                <td>fluente</td>
+
+
+
+            </tr>
+
+
+        </table>
+
+
+
+    </nav>
+
+    <nav id="formacao">
+        <h2 id="Fa">
+            Formacao academica
+
+        </h2>
+
+        2003-2007
+
+        <table border="1">
+            <tr>
+                <th>Anos</th>
+                <th>2003-2007</th>
+                <th>2007-2009</th>
+                <th>2010-2012</th>
+                <th>2013-2014</th>
+
+            </tr>
+            <tr>
+                <td>nivel acadmico</td>
+                <td> Ensino primario do 1° grau</td>
+                <td> Ensino primario do 2° grau</td>
+                <td> Ensino Basico </td>
+                <td> Ensino Medio </td>
+            </tr>
+            <tr>
+                <td> Instituicao de ensino</td>
+                <td>EPC de Inharrime</td>
+                <td> EPC de Inharrime </td>
+                <td> Escola Secundaria 04 de Outubro de Inharrime </td>
+                <td> Escola Secundaria Laura vicuna de inharrime </td>
+
+            </tr>
+
+
+        </table>
+    </nav>
+
+</div>
+</body>
+</html>
